cc432d51ba2726874e3c4f25b76fb9dcc4878b3e
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-22 16:51:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,6 +32,11 @@ end
 
 def merge_data(keys, data)
   merged_data = []
-  keys.each {|i| data.first.each {|key,value| if i.values[0] == key then merged_data << i.merge(value) end}}
+  keys.each {|i| data.first.each {|key,value| 
+    if i.values[0] == key 
+      then merged_data << i.merge(value) 
+    end
+   }
+  }
   merged_data
 end
