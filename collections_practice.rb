ab619841478a2d15d12a54f8e6569522ba3f546f
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-22 16:34:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
-# your code goes here
+def begins_with_r(tools)
+  tools.all? { |item| item.start_with? ("r") }
+end
+
+def contain_a(tools)
+  tools.select { |word| word.include? ("a") }
+end
+
+def first_wa(elements)
+  elements.find { |word| word.to_s.start_with?("wa") }
+end
+
+def remove_non_strings(array)
+  array.delete_if { |element| element.class != String }
+end
+
+def count_elements(array)
+array.count { |duplicates| duplicates % 2 == 0 }
+end
